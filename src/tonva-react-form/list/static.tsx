d12228216ee197b859668f67ba804c0267663c88
Henry <liaohengyi@outlook.com>
2018-12-28 22:30:22 +0800

    build

---
+++ src/tonva-react-form/list/static.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as classNames from 'classnames';
+import {ListBase} from './base';
+import { uid } from '../uid';
+
+export class Static extends ListBase {
+    render(item:any, index:number):JSX.Element {
+        let {className, key, render} = this.list.props.item;
+        if (typeof item === 'string') {
+            let cn = classNames('va-list-gap', 'px-3', 'pt-1');
+            return <li className={cn}>{item}</li>;
+        }
+        return <li className={classNames(className)}>
+            {this.renderContent(item, index)}
+        </li>
+    }
+}
